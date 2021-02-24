ff90147884b2dac306592de57fe299bb2b0cbf9b
Amarnath K <amarnathk5674@gmail.com>
2021-02-24 23:35:02 +0530

    added csv writer

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,8 +1,13 @@
 import random
-
+import csv
+hint = input("Enter the hint for where the password is applied:")
 n = int(input("Enter the number of characters required:"))
 print("Your password: ",end="")
+password = ""
 for i in range(n):
     x = random.randint(33,126)
-    print(chr(x),end="")
-print("")
+    password += chr(x)
+print(password)
+with open("passwords.csv","w") as file:
+    writer = csv.writer(file)
+    writer.writerow([hint,password])
